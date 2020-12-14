35a62fdf8791ae9f73a99f2353031111aa7ee27c
ksy781 <k.s.k.s.hanahana@gmail.com>
2020-12-15 00:07:47 +0900

    Initial Commit

---
+++ task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task1
+//
+//  Created by 酒井健太郎 on 2020/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
