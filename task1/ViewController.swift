d9c2bb15d93711ad650ef8da924eec951eca08ca
akio0911 <akio0911@gmail.com>
2020-12-15 10:13:42 +0900

    feedback

--- task1/ViewController.swift
+++ task1/ViewController.swift
@@ -7,38 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var textField3: UITextField!
-    @IBOutlet weak var textField4: UITextField!
-    @IBOutlet weak var textField5: UITextField!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var textField3: UITextField!
+    @IBOutlet private weak var textField4: UITextField!
+    @IBOutlet private weak var textField5: UITextField!
     
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet private weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 初期値
         label.text = "0"
-        
     }
     
     @IBAction func plus(_ sender: UIButton) {
+        let textFields: [UITextField] = [
+            textField1,
+            textField2,
+            textField3,
+            textField4,
+            textField5
+        ]
         
-        // 入力
-        // UITextFieldがOptional<String>型のため??演算子が必要
-        let num1 = Int(textField1.text ?? "") ?? 0
-        let num2 = Int(textField2.text ?? "") ?? 0
-        let num3 = Int(textField3.text ?? "") ?? 0
-        let num4 = Int(textField4.text ?? "") ?? 0
-        let num5 = Int(textField5.text ?? "") ?? 0
-        // 処理
-        let result = num1 + num2 + num3 + num4 + num5
+        var result = 0
+        
+        for textField in textFields {
+            // UITextFieldがOptional<String>型のため??演算子が必要
+            result += Int(textField.text ?? "") ?? 0
+        }
+        
         // 出力
         label.text = String(result)
     }
-    
 }
-
